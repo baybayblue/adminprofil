af654600b62ef714445e4ea745d7c7a0ece2c4b4
baybayblue <arindaayulestari.31@gmail.com>
2025-08-12 07:33:23 +0700

    halaman tentang sekolah

--- resources/views/interface/profile/tentang.blade.php
+++ resources/views/interface/profile/tentang.blade.php
@@ -8,7 +8,7 @@
         <div class="row">
             <div class="col-md-12">
                 <div class="breadcrumb-text">
-                    <h1 class="text-center">Tentang Sekolah Kami</h1>
+                    <h1 class="text-center">Tentang SMK Kami</h1>
                     <div class="breadcrumb-bar">
                         <ul class="breadcrumb">
                             <li><a href="{{ url('/') }}">Beranda</a></li>
@@ -29,37 +29,37 @@
             <div class="col-lg-3 col-md-6 col-sm-6 activity">
                 <div class="single-activity">
                     <div class="single-activity-icon">
-                        <i class="fa fa-graduation-cap"></i>
+                        <i class="fa fa-industry"></i>
                     </div>
-                    <h4>Akademik Unggul</h4>
-                    <p>Kurikulum berbasis kompetensi dengan metode pembelajaran modern untuk mengoptimalkan potensi siswa.</p>
+                    <h4>Link and Match Industri</h4>
+                    <p>Kurikulum berbasis kompetensi yang diselaraskan dengan kebutuhan dunia kerja.</p>
                 </div>
             </div>
             <div class="col-lg-3 col-md-6 col-sm-6 activity">
                 <div class="single-activity">
                     <div class="single-activity-icon">
-                        <i class="fa fa-users"></i>
+                        <i class="fa fa-certificate"></i>
                     </div>
-                    <h4>Guru Berkualitas</h4>
-                    <p>Didukung oleh tenaga pendidik profesional yang berkompeten di bidangnya masing-masing.</p>
+                    <h4>Sertifikasi Kompetensi</h4>
+                    <p>Program sertifikasi berbasis industri untuk meningkatkan kompetensi lulusan.</p>
                 </div>
             </div>
             <div class="col-lg-3 col-md-6 col-sm-6 activity">
                 <div class="single-activity mrg-res-top-md">
                     <div class="single-activity-icon">
-                        <i class="fa fa-futbol-o"></i>
+                        <i class="fa fa-briefcase"></i>
                     </div>
-                    <h4>Ekstrakurikuler</h4>
-                    <p>Beragam kegiatan ekstrakurikuler untuk mengembangkan bakat dan minat siswa.</p>
+                    <h4>Teaching Factory</h4>
+                    <p>Pembelajaran berbasis produksi untuk pengalaman kerja nyata.</p>
                 </div>
             </div>
             <div class="col-lg-3 col-md-6 col-sm-6 activity">
                 <div class="single-activity mrg-res-top-md">
                     <div class="single-activity-icon">
-                        <i class="fa fa-heart"></i>
+                        <i class="fa fa-laptop"></i>
                     </div>
-                    <h4>Pendidikan Karakter</h4>
-                    <p>Pembentukan karakter siswa melalui nilai-nilai budi pekerti dan keagamaan.</p>
+                    <h4>Teknologi Modern</h4>
+                    <p>Fasilitas pembelajaran berbasis teknologi terkini.</p>
                 </div>
             </div>
         </div>
@@ -74,8 +74,8 @@
             <div class="col-md-12">
                 <div class="section-title-wrapper">
                     <div class="section-title">
-                        <h3>Profil Sekolah</h3>
-                        <p>Mengenal lebih dekat tentang sekolah kami</p>
+                        <h3>Profil SMK Kami</h3>
+                        <p>Identitas dan sejarah singkat sekolah kami</p>
                     </div>
                 </div> 
             </div>       
@@ -83,37 +83,34 @@
         <div class="row">
             <div class="col-lg-6">
                 <div class="about-text-container">
-                    <p><span>Selamat datang di {{ $profil->nama_sekolah ?? 'Sekolah Kami' }}</span> {{ $profil->sejarah ?? 'Sekolah kami berdiri dengan komitmen untuk memberikan pendidikan terbaik bagi generasi penerus bangsa. Dengan fasilitas yang memadai dan lingkungan yang kondusif, kami bertekad menjadi lembaga pendidikan unggulan di daerah ini.' }}</p>
-                    
-                    <div class="about-us">
-                        <span><i class="fa fa-check"></i> NPSN: {{ $profil->npsn ?? '12345678' }}</span>
-                        <span><i class="fa fa-check"></i> Akreditasi: {{ $profil->akreditasi ?? 'A (Unggul)' }}</span>
-                        <span><i class="fa fa-check"></i> Tahun Berdiri: {{ $profil->tahun_berdiri ?? '1980' }}</span>
-                        <span><i class="fa fa-check"></i> Jumlah Siswa: {{ $profil->jumlah_siswa ?? '500' }} siswa</span>
-                        <span><i class="fa fa-check"></i> Jumlah Guru: {{ $profil->jumlah_guru ?? '30' }} guru</span>
-                        <span><i class="fa fa-check"></i> Ruang Kelas: {{ $profil->jumlah_ruang_kelas ?? '15' }} ruang</span>
+                    <div class="text-center mb-4">
+                        <img src="{{ asset('storage/' . ($profil->logo ?? 'default/logo.png')) }}" alt="Logo SMK" class="img-fluid" style="max-height: 150px;">
                     </div>
                     
-                    <div class="mt-4">
-                        <a href="{{ route('profil.visi-misi') }}" class="btn btn-primary">
-                            <i class="fa fa-book"></i> Baca Visi & Misi Sekolah
-                        </a>
+                    <h4 class="mb-3">{{ $profil->nama_sekolah ?? 'SMK Contoh' }}</h4>
+                    <p>{{ $profil->sejarah ?? 'SMK kami berdiri dengan komitmen untuk menyiapkan tenaga kerja terampil yang siap bersaing di dunia industri. Dengan fasilitas praktikum yang memadai dan kerjasama yang kuat dengan dunia usaha/dunia industri, kami bertekad menjadi lembaga pendidikan vokasi unggulan.' }}</p>
+                    
+                    <div class="about-us">
+                        <span><i class="fa fa-check-circle"></i> <strong>NPSN:</strong> {{ $profil->npsn ?? '12345678' }}</span>
+                        <span><i class="fa fa-check-circle"></i> <strong>Akreditasi:</strong> {{ $profil->akreditasi ?? 'A (Unggul)' }}</span>
+                        <span><i class="fa fa-check-circle"></i> <strong>Tahun Berdiri:</strong> {{ $profil->tahun_berdiri ?? '1990' }}</span>
+                        <span><i class="fa fa-check-circle"></i> <strong>Alamat:</strong> {{ $profil->alamat ?? 'Jl. Contoh No. 123' }}</span>
                     </div>
                 </div>
             </div>
             <div class="col-lg-6">
                 <div class="row">
                     <div class="col-md-6 mb-3">
-                        <img src="{{ asset('img/about/classroom.jpg') }}" alt="Ruang Kelas" class="img-fluid rounded">
+                        <img src="{{ asset('img/about/workshop.jpg') }}" alt="Workshop Praktik" class="img-fluid rounded shadow">
                     </div>
                     <div class="col-md-6 mb-3">
-                        <img src="{{ asset('img/about/lab.jpg') }}" alt="Laboratorium" class="img-fluid rounded">
+                        <img src="{{ asset('img/about/lab-komputer.jpg') }}" alt="Lab Komputer" class="img-fluid rounded shadow">
                     </div>
                     <div class="col-md-6 mb-3">
-                        <img src="{{ asset('img/about/library.jpg') }}" alt="Perpustakaan" class="img-fluid rounded">
+                        <img src="{{ asset('img/about/teaching-factory.jpg') }}" alt="Teaching Factory" class="img-fluid rounded shadow">
                     </div>
                     <div class="col-md-6 mb-3">
-                        <img src="{{ asset('img/about/sport.jpg') }}" alt="Lapangan Olahraga" class="img-fluid rounded">
+                        <img src="{{ asset('img/about/siswa-praktik.jpg') }}" alt="Siswa Praktik" class="img-fluid rounded shadow">
                     </div>
                 </div>
             </div>
@@ -122,6 +119,40 @@
 </div>
 <!-- End of Profil Sekolah -->
 
+<!-- Visi Misi Start -->
+<div class="skill-and-experience-area bg-light">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="section-title-wrapper">
+                    <div class="section-title">
+                        <h3>Visi & Misi</h3>
+                        <p>Landasan dalam penyelenggaraan pendidikan</p>
+                    </div>
+                </div> 
+            </div>       
+        </div>
+        <div class="row">
+            <div class="col-lg-6">
+                <div class="vision-box">
+                    <h4><i class="fa fa-bullseye"></i> Visi</h4>
+                    <p>{{ $profil->visi ?? '"Menjadi SMK unggulan yang menghasilkan lulusan kompeten, berkarakter, dan siap bersaing di dunia kerja global."' }}</p>
+                </div>
+            </div>
+            <div class="col-lg-6">
+                <div class="mission-box">
+                    <h4><i class="fa fa-tasks"></i> Misi</h4>
+                    <p>{{ $profil->misi ?? '1. Menyelenggarakan pendidikan vokasi berbasis kompetensi<br>
+                    2. Mengembangkan kerjasama dengan dunia usaha/industri<br>
+                    3. Membentuk karakter peserta didik yang berakhlak mulia<br>
+                    4. Menerapkan budaya kerja industri di lingkungan sekolah' }}</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- End of Visi Misi -->
+
 <!-- Fasilitas Sekolah Start -->
 <div class="skill-and-experience-area">
     <div class="container">
@@ -129,8 +160,8 @@
             <div class="col-md-12">
                 <div class="section-title-wrapper">
                     <div class="section-title">
-                        <h3>Fasilitas Sekolah</h3>
-                        <p>Dukungan infrastruktur untuk proses belajar mengajar</p>
+                        <h3>Fasilitas Praktik</h3>
+                        <p>Dukungan infrastruktur untuk pembelajaran vokasi</p>
                     </div>
                 </div> 
             </div>       
@@ -139,55 +170,55 @@
             <div class="col-md-4 mb-4">
                 <div class="facility-card">
                     <div class="facility-icon">
-                        <i class="fa fa-building"></i>
+                        <i class="fa fa-cogs"></i>
                     </div>
-                    <h4>Ruang Kelas Nyaman</h4>
-                    <p>Ruang kelas dilengkapi AC, LCD projector, dan furniture ergonomis untuk kenyamanan belajar.</p>
+                    <h4>Workshop Praktik</h4>
+                    <p>Bengkel kerja dengan peralatan standar industri untuk praktik siswa.</p>
                 </div>
             </div>
             <div class="col-md-4 mb-4">
                 <div class="facility-card">
                     <div class="facility-icon">
-                        <i class="fa fa-flask"></i>
+                        <i class="fa fa-laptop-code"></i>
                     </div>
-                    <h4>Laboratorium Lengkap</h4>
-                    <p>Lab komputer, IPA, dan bahasa dengan peralatan modern untuk praktikum siswa.</p>
+                    <h4>Lab Komputer</h4>
+                    <p>Laboratorium komputer dengan spesifikasi tinggi untuk pembelajaran IT.</p>
                 </div>
             </div>
             <div class="col-md-4 mb-4">
                 <div class="facility-card">
                     <div class="facility-icon">
-                        <i class="fa fa-book"></i>
+                        <i class="fa fa-network-wired"></i>
                     </div>
-                    <h4>Perpustakaan Digital</h4>
-                    <p>Koleksi buku lengkap dengan sistem digital untuk kemudahan akses literatur.</p>
+                    <h4>Lab Jaringan</h4>
+                    <p>Peralatan jaringan komputer untuk praktik TKJ dan RPL.</p>
                 </div>
             </div>
             <div class="col-md-4 mb-4">
                 <div class="facility-card">
                     <div class="facility-icon">
-                        <i class="fa fa-futbol-o"></i>
+                        <i class="fa fa-car"></i>
                     </div>
-                    <h4>Lapangan Olahraga</h4>
-                    <p>Lapangan basket, voli, futsal, dan atletik untuk kegiatan olahraga siswa.</p>
+                    <h4>Bengkel Otomotif</h4>
+                    <p>Bengkel lengkap untuk praktik teknik kendaraan ringan.</p>
                 </div>
             </div>
             <div class="col-md-4 mb-4">
                 <div class="facility-card">
                     <div class="facility-icon">
-                        <i class="fa fa-wifi"></i>
+                        <i class="fa fa-robot"></i>
                     </div>
-                    <h4>Internet Cepat</h4>
-                    <p>Jaringan WiFi kecepatan tinggi di seluruh area sekolah untuk menunjang pembelajaran.</p>
+                    <h4>Lab Mekatronika</h4>
+                    <p>Fasilitas praktik untuk teknik mekatronika dan otomasi industri.</p>
                 </div>
             </div>
             <div class="col-md-4 mb-4">
                 <div class="facility-card">
                     <div class="facility-icon">
-                        <i class="fa fa-medkit"></i>
+                        <i class="fa fa-chalkboard-teacher"></i>
                     </div>
-                    <h4>Klinik Kesehatan</h4>
-                    <p>UKS dengan tenaga medis untuk penanganan kesehatan dasar siswa.</p>
+                    <h4>Teaching Factory</h4>
+                    <p>Unit produksi yang menerapkan sistem kerja industri sebenarnya.</p>
                 </div>
             </div>
         </div>
@@ -202,8 +233,8 @@
             <div class="col-md-12">
                 <div class="section-title-wrapper">
                     <div class="section-title">
-                        <h3>Prestasi Sekolah</h3>
-                        <p>Beberapa pencapaian yang membanggakan</p>
+                        <h3>Prestasi & Pencapaian</h3>
+                        <p>Beberapa bukti kualitas pendidikan kami</p>
                     </div>
                 </div> 
             </div>       
@@ -212,28 +243,28 @@
             <div class="col-md-4">
                 <div class="achievement-box text-center">
                     <div class="achievement-number">
-                        <span class="counter" data-count="{{ $profil->tahun_berdiri ? now()->year - $profil->tahun_berdiri : 30 }}">0</span>+
+                        <span class="counter" data-count="{{ $profil->tahun_berdiri ? now()->year - $profil->tahun_berdiri : 20 }}">0</span>+
                     </div>
                     <h4>Tahun Pengalaman</h4>
-                    <p>Melayani dunia pendidikan dengan dedikasi tinggi</p>
+                    <p>Menyelenggarakan pendidikan vokasi berkualitas</p>
                 </div>
             </div>
             <div class="col-md-4">
                 <div class="achievement-box text-center">
                     <div class="achievement-number">
-                        <span class="counter" data-count="{{ $profil->jumlah_siswa ?? 500 }}">0</span>+
+                        <span class="counter" data-count="{{ $profil->jumlah_siswa ?? 800 }}">0</span>+
                     </div>
                     <h4>Siswa Aktif</h4>
-                    <p>Generasi penerus bangsa yang sedang dibina</p>
+                    <p>Calon tenaga kerja terampil yang sedang dibina</p>
                 </div>
             </div>
             <div class="col-md-4">
                 <div class="achievement-box text-center">
                     <div class="achievement-number">
-                        <span class="counter" data-count="{{ $profil->jumlah_guru ?? 30 }}">0</span>+
+                        <span class="counter" data-count="{{ $profil->jumlah_guru ?? 40 }}">0</span>+
                     </div>
-                    <h4>Guru Profesional</h4>
-                    <p>Tenaga pendidik yang berdedikasi tinggi</p>
+                    <h4>Guru & Instruktur</h4>
+                    <p>Tenaga pendidik profesional dan berpengalaman</p>
                 </div>
             </div>
         </div>
@@ -252,6 +283,7 @@
         box-shadow: 0 5px 15px rgba(0,0,0,0.05);
         height: 100%;
         transition: all 0.3s ease;
+        text-align: center;
     }
     .facility-card:hover {
         transform: translateY(-5px);
@@ -280,6 +312,21 @@
     }
     .about-us i {
         color: #f39c12;
+        margin-right: 10px;
+    }
+    .vision-box, .mission-box {
+        background: #fff;
+        padding: 30px;
+        border-radius: 5px;
+        box-shadow: 0 5px 15px rgba(0,0,0,0.05);
+        height: 100%;
+    }
+    .vision-box h4 i {
+        color: #3498db;
+        margin-right: 10px;
+    }
+    .mission-box h4 i {
+        color: #2ecc71;
         margin-right: 10px;
     }
 </style>
